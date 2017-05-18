fe407b4d1ae81b6cd797d228dbe5ef074b1c3f0a
yang.weicheng <445919439@qq.com>
2017-05-18 09:49:23 +0800

    Code style

--- YWCRangeSlider/RangeSlider.swift
+++ YWCRangeSlider/RangeSlider.swift
@@ -17,41 +17,41 @@ class RangeSlider: UIControl {
     /// default 1.0
     var maximumValue = 1.0
     
-    /// defalut 0.0 The minimum distance between the lower value and higher value
+    /// defalut 0.0 The minimum distance between the low value and higher value
     var minimumRange = 0.0
     
     var stepValueContinuously = false
     
-    var lowerValue = 0.0
+    var lowValue = 0.0
     var higherValue = 0.0
     
     ///  maximum value for left thumb
-    var lowerMaximumValue = 0.0;
+    var lowMaximumValue = 0.0
     
     /// minimum value for right thumb
-    var higherMinimumValue = 0.0;
+    var higherMinimumValue = 0.0
     
-    var lowerTouchEdgeInsets = UIEdgeInsetsMake(-5, -5, -5, -5)
+    var lowTouchEdgeInsets = UIEdgeInsetsMake(-5, -5, -5, -5)
     let higherTouchEdgeInsets = UIEdgeInsetsMake(-5, -5, -5, -5)
     
-    var trackImageView: UIImageView!;
-    var trackBackgroundImageView: UIImageView!;
-    var lowerHandle: UIImageView!;
-    var higherHandle: UIImageView!;
-    private var lowCenter: CGPoint!;
-    private var higherCenter: CGPoint!;
+    var trackImageView: UIImageView!
+    var trackBackgroundImageView: UIImageView!
+    var lowHandle: UIImageView!
+    var higherHandle: UIImageView!
+    private var lowCenter: CGPoint!
+    private var higherCenter: CGPoint!
     
     
     
-    var defaultHandleImage: UIImage = #imageLiteral(resourceName: "slider-default7-handle.png");
+    var defaultHandleImage: UIImage = #imageLiteral(resourceName: "slider-default7-handle.png")
     
     
     
     
     
     override init(frame: CGRect) {
-        super.init(frame: frame);
-        configureViews();
+        super.init(frame: frame)
+        configureViews()
     }
     
     
@@ -65,10 +65,11 @@ class RangeSlider: UIControl {
     func configureViews() {
         
 
-        lowerValue = minimumValue
+        lowValue = minimumValue
         higherValue = maximumValue
-        lowerMaximumValue = Double.nan
+        lowMaximumValue = Double.nan
         higherMinimumValue = Double.nan
+        
         
         
         
@@ -79,12 +80,12 @@ class RangeSlider: UIControl {
         trackBackgroundImageView.backgroundColor = .blue
         addSubview(self.trackBackgroundImageView)
         
-        lowerHandle = UIImageView(image: defaultHandleImage)
-        lowerHandle.frame = CGRect(x: 40, y: 20, width: 20, height: 20);
-        addSubview(lowerHandle)
+        lowHandle = UIImageView(image: defaultHandleImage)
+        lowHandle.frame = CGRect(x: 40, y: 20, width: 20, height: 20)
+        addSubview(lowHandle)
         
         higherHandle = UIImageView(image: defaultHandleImage)
-        higherHandle.frame = CGRect(x: 200, y: 20, width: 20, height: 20);
+        higherHandle.frame = CGRect(x: 200, y: 20, width: 20, height: 20)
         addSubview(higherHandle)
         
         
@@ -94,9 +95,9 @@ class RangeSlider: UIControl {
     
     override func beginTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
         let touchPoint = touch.location(in: self)
-        let lowerRect = UIEdgeInsetsInsetRect(lowerHandle.frame, lowerTouchEdgeInsets)
-        if lowerRect.contains(touchPoint) {
-            lowerHandle.isHighlighted = true
+        let lowRect = UIEdgeInsetsInsetRect(lowHandle.frame, lowTouchEdgeInsets)
+        if lowRect.contains(touchPoint) {
+            lowHandle.isHighlighted = true
             
         }
         
@@ -109,7 +110,7 @@ class RangeSlider: UIControl {
     }
     
     override func continueTracking(_ touch: UITouch, with event: UIEvent?) -> Bool {
-        if lowerHandle.isHighlighted == false && higherHandle.isHighlighted == false {
+        if lowHandle.isHighlighted == false && higherHandle.isHighlighted == false {
             return true
         }
         let touchPoint = touch.location(in: self)
@@ -118,29 +119,29 @@ class RangeSlider: UIControl {
         
         
         
-        if lowerHandle.isHighlighted {
-            let newValue = lowerValueForCenterX(x: Double(touchPoint.x))
-            if newValue < lowerValue || !higherHandle.isHighlighted {
+        if lowHandle.isHighlighted {
+            let newValue = lowValueForCenterX(x: Double(touchPoint.x))
+            if newValue < lowValue || !higherHandle.isHighlighted {
                 higherHandle.isHighlighted = false
-                bringSubview(toFront: lowerHandle)
+                bringSubview(toFront: lowHandle)
                 
                 let pointX = touchPoint.x
-                let pointY = lowerHandle.center.y
-                lowerHandle.center = CGPoint(x: pointX, y: pointY)
+                let pointY = lowHandle.center.y
+                lowHandle.center = CGPoint(x: pointX, y: pointY)
                 
             } else {
-                lowerHandle.isHighlighted = false
+                lowHandle.isHighlighted = false
             }
             
         }
         
         if higherHandle.isHighlighted {
             
-            let newValue = lowerValueForCenterX(x: Double(touchPoint.x))
+            let newValue = lowValueForCenterX(x: Double(touchPoint.x))
             
-            if newValue > higherValue || !lowerHandle.isHighlighted {
+            if newValue > higherValue || !lowHandle.isHighlighted {
                 
-                lowerHandle.isHighlighted = false
+                lowHandle.isHighlighted = false
                 bringSubview(toFront: higherHandle)
                 
                 let pointX = touchPoint.x
@@ -153,23 +154,23 @@ class RangeSlider: UIControl {
             
         }
         
-        return true;
+        return true
     }
     
     
     override func endTracking(_ touch: UITouch?, with event: UIEvent?) {
-        lowerHandle.isHighlighted = false
+        lowHandle.isHighlighted = false
         higherHandle.isHighlighted = false
     }
     
     
     
-    func lowerValueForCenterX(x:Double) -> Double {
+    func lowValueForCenterX(x:Double) -> Double {
         
         
         
         
-        let padding = Double(lowerHandle.frame.size.width/2.0)
+        let padding = Double(lowHandle.frame.size.width/2.0)
         let valueGap = maximumValue - minimumValue
         let lengthMinusPadding = Double(self.frame.size.width) - padding * 2
         
@@ -186,7 +187,7 @@ class RangeSlider: UIControl {
 //        
 //    }
     
-    func setValue(lower:Double, higher:Double, animated:Bool) {
+    func setValue(low:Double, higher:Double, animated:Bool) {
         
         
         
@@ -194,24 +195,24 @@ class RangeSlider: UIControl {
         
     }
     
-    func setLowerValue(value:Double, animated:Bool) {
-        setValue(lower: lowerValue, higher: Double.nan, animated: true)
+    func setlowValue(value:Double, animated:Bool) {
+        setValue(low: lowValue, higher: Double.nan, animated: true)
     }
     
     func setHigherValue(value:Double, animated:Bool) {
-        setValue(lower: Double.nan, higher: value, animated: true)
+        setValue(low: Double.nan, higher: value, animated: true)
     }
     
-    func setLowerValue(lowerValue:Double) {
-        var value = lowerValue
+    func setlowValue(lowValue:Double) {
+        var value = lowValue
         value = min(value, maximumValue)
         value = max(value, minimumValue)
         
-        if !lowerMaximumValue.isNaN {
-            value = min(value, lowerMaximumValue)
+        if !lowMaximumValue.isNaN {
+            value = min(value, lowMaximumValue)
         }
         value = min(value, higherValue - minimumRange)
-        self.lowerValue = value
+        self.lowValue = value
         
         setNeedsDisplay()
     }
@@ -223,7 +224,7 @@ class RangeSlider: UIControl {
         if !higherMinimumValue.isNaN {
             value = max(value, higherMinimumValue)
         }
-        value = max(value, lowerValue + minimumRange)
+        value = max(value, lowValue + minimumRange)
         self.higherValue = value
         setNeedsDisplay()
     }
