db4c560bcc4936bd275b97a380afcf4e9f3e53b5
yang.weicheng <445919439@qq.com>
2017-05-20 11:45:11 +0800

    Create a tableView

--- YWCRangeSlider/ViewController.swift
+++ YWCRangeSlider/ViewController.swift
@@ -8,23 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "YWCRangeSlider"
         
-        let slider = RangeSlider(frame: CGRect(x: 50, y: 50, width: 300, height: 50));
-        view.addSubview(slider);
-        slider.backgroundColor = .white;
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        let tableView = UITableView(frame: self.view.frame, style: .plain)
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 100
+        
+        let basicSlider = RangeSlider(frame: CGRect(x: 50, y: 50, width: 300, height: 50));
+        view.addSubview(basicSlider);
+        
+        let minimumRangeNegativeSlider = RangeSlider(frame: CGRect(x: 50, y: 100, width: 300, height: 50));
+        minimumRangeNegativeSlider.minimumRange = -1;
+        view.addSubview(minimumRangeNegativeSlider);
+        
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell!.textLabel?.text = "fsadf"
+        return cell!
+    }
 }
