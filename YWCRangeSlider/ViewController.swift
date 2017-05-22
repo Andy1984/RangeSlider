98677f9a9d5d3a28f4ef903712d7a7a5c313017e
yang.weicheng <445919439@qq.com>
2017-05-22 10:12:20 +0800

    Delete the tableView

--- YWCRangeSlider/ViewController.swift
+++ YWCRangeSlider/ViewController.swift
@@ -8,39 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "YWCRangeSlider"
         
-        let tableView = UITableView(frame: self.view.frame, style: .plain)
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        tableView.rowHeight = 100
-        
-        let basicSlider = RangeSlider(frame: CGRect(x: 50, y: 50, width: 300, height: 50));
+        let basicSlider = RangeSlider(frame: CGRect(x: 50, y: 64, width: 300, height: 50));
         view.addSubview(basicSlider);
         
-        let minimumRangeNegativeSlider = RangeSlider(frame: CGRect(x: 50, y: 100, width: 300, height: 50));
+        let minimumRangeNegativeSlider = RangeSlider(frame: CGRect(x: 50, y: 64 + 50, width: 300, height: 50));
         minimumRangeNegativeSlider.minimumRange = -1;
         view.addSubview(minimumRangeNegativeSlider);
         
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        if cell == nil {
-            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
-        }
-        cell!.textLabel?.text = "fsadf"
-        return cell!
-    }
 }
