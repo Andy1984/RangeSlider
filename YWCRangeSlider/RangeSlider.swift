f39bf23476e0ea1a87c19b780597adf7ec879c17
yang.weicheng <445919439@qq.com>
2017-05-31 22:52:23 +0800

    Set up basic slider in tableView

--- YWCRangeSlider/RangeSlider.swift
+++ YWCRangeSlider/RangeSlider.swift
@@ -185,7 +185,8 @@ class RangeSlider: UIControl {
     
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        configureViews()
     }
     
     func trackImageForCurrentValues() -> UIImage {
--- YWCRangeSlider/ViewController.swift
+++ YWCRangeSlider/ViewController.swift
@@ -10,23 +10,24 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    @IBOutlet weak var basicSlider: RangeSlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "YWCRangeSlider"
-        
-        let basicSlider = RangeSlider(frame: CGRect(x: 50, y: 264, width: 300, height: 50));
-        basicSlider.isLabelHidden = false
-        view.addSubview(basicSlider);
-        
-        let minimumRangeNegativeSlider = RangeSlider(frame: CGRect(x: 50, y: 64 + 50, width: 300, height: 50));
-        minimumRangeNegativeSlider.minimumDistance = -1;
-        view.addSubview(minimumRangeNegativeSlider);
-        
-        let stepRangSlider = RangeSlider(frame: CGRect(x: 50, y: 64 + 100, width: 300, height: 50))
-        stepRangSlider.stepValue = 0.2
-        stepRangSlider.isMovingStepByStep = true
-        view.addSubview(stepRangSlider)
+//        title = "YWCRangeSlider"
+//        
+//        let basicSlider = RangeSlider(frame: CGRect(x: 50, y: 264, width: 300, height: 50));
+//        basicSlider.isLabelHidden = false
+//        view.addSubview(basicSlider);
+//        
+//        let minimumRangeNegativeSlider = RangeSlider(frame: CGRect(x: 50, y: 64 + 50, width: 300, height: 50));
+//        minimumRangeNegativeSlider.minimumDistance = -1;
+//        view.addSubview(minimumRangeNegativeSlider);
+//        
+//        let stepRangSlider = RangeSlider(frame: CGRect(x: 50, y: 64 + 100, width: 300, height: 50))
+//        stepRangSlider.stepValue = 0.2
+//        stepRangSlider.isMovingStepByStep = true
+//        view.addSubview(stepRangSlider)
     }
     
 }
